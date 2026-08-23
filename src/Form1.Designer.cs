9a121d3d2c61c1a97fcd9440c6f880d06d784769
Mantounio <mahdidev83@gmail.com>
2026-08-23 18:20:45 +0330

    now the main window's location can change base on the mouse position on window's screen

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            lbl_mouseUP_info = new Label();
+            lbl_mouseDOWN_info = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(176, 175);
+            button1.Name = "button1";
+            button1.Size = new Size(165, 29);
+            button1.TabIndex = 0;
+            button1.Text = "mouse position";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(176, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(165, 29);
+            button2.TabIndex = 1;
+            button2.Text = "e.x & e.y pos";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(447, 237);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 20);
+            label1.TabIndex = 2;
+            label1.Text = "information";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(447, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 20);
+            label2.TabIndex = 3;
+            label2.Text = "size of window : 962, 634";
+            // 
+            // lbl_mouseUP_info
+            // 
+            lbl_mouseUP_info.AutoSize = true;
+            lbl_mouseUP_info.Location = new Point(447, 353);
+            lbl_mouseUP_info.Name = "lbl_mouseUP_info";
+            lbl_mouseUP_info.Size = new Size(87, 20);
+            lbl_mouseUP_info.TabIndex = 4;
+            lbl_mouseUP_info.Text = "information";
+            // 
+            // lbl_mouseDOWN_info
+            // 
+            lbl_mouseDOWN_info.AutoSize = true;
+            lbl_mouseDOWN_info.Location = new Point(447, 409);
+            lbl_mouseDOWN_info.Name = "lbl_mouseDOWN_info";
+            lbl_mouseDOWN_info.Size = new Size(87, 20);
+            lbl_mouseDOWN_info.TabIndex = 5;
+            lbl_mouseDOWN_info.Text = "information";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(962, 634);
+            Controls.Add(lbl_mouseDOWN_info);
+            Controls.Add(lbl_mouseUP_info);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label lbl_mouseUP_info;
+        private Label lbl_mouseDOWN_info;
     }
 }
--- src/Form1.cs
+++ src/Form1.cs
@@ -4,15 +4,48 @@ namespace wordz.src;
 
 public partial class Form1 : Form
 {
-    [DllImport("gdi32.dll", EntryPoint = "CreateRoundRectRgn"),]
+    [DllImport("gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
     static extern IntPtr CreateRoundRectRgn(int x1, int y1, int x2, int y2,
     int cx, int cy);
 
+    int down_num = 0;
+    int up_num = 0;
 
-
+    int COORX;
+    int COORY;
+    bool MOV_WIND_LOCATION;
+    
+    
     public Form1()
     {
         InitializeComponent();
-        Region = Region.FromHrgn(CreateRoundRectRgn(0,0,Width,Height,50,50));
+        Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 50, 50));
+    }
+
+    private void Form1_MouseMove(object sender, MouseEventArgs e)
+    {
+        label1.Text = $"x = {e.X} : y = {e.Y} of mouse coordination (mouse moving event)";
+
+        if (MOV_WIND_LOCATION)
+            SetDesktopLocation(MousePosition.X - COORX, MousePosition.Y - COORY);
+
+    }
+
+    private void Form1_MouseUp(object sender, MouseEventArgs e)
+    {
+        lbl_mouseUP_info.Text = $"mouse is upped (mouse UP event) | {up_num}";
+        up_num++;
+
+        MOV_WIND_LOCATION = false;
+    }
+
+    private void Form1_MouseDown(object sender, MouseEventArgs e)
+    {
+        MOV_WIND_LOCATION = true;
+        COORX = e.X;
+        COORY = e.Y;
+
+        lbl_mouseDOWN_info.Text = $"mouse is down (mouse down event) | {down_num}";
+        down_num++;
     }
 }
