98dc1427ade04953fec157111e25e3631f9cecee
Mantounio <mahdidev83@gmail.com>
2026-09-05 13:19:59 +0330

    finished the ui's overall design

--- src/Main Page/main_page.Designer.cs
+++ src/Main Page/main_page.Designer.cs
@@ -91,11 +91,12 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(37, 34);
+            label1.Font = new Font("Roboto", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(17, 24);
             label1.Name = "label1";
-            label1.Size = new Size(69, 20);
+            label1.Size = new Size(163, 53);
             label1.TabIndex = 5;
-            label1.Text = "&#128276;";
+            label1.Text = "•Wordz";
             // 
             // main_page
             // 
