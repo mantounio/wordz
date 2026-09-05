d2c2063ad717b898ee0693a28f563ccbd8763ee9
Mantounio <mahdidev83@gmail.com>
2026-09-05 16:49:50 +0330

    'btn_addword' can open its page(usercontrol)

--- src/Main Page/main_page.Designer.cs
+++ src/Main Page/main_page.Designer.cs
@@ -28,65 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
+            btn_takequiz = new Button();
+            btn_addword = new Button();
+            btn_setting = new Button();
+            btn_wordslist = new Button();
             label1 = new Label();
             SuspendLayout();
             // 
-            // button1
+            // btn_takequiz
             // 
-            button1.BackColor = SystemColors.Control;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 15F);
-            button1.Image = Properties.Resources.l_quiz_75px;
-            button1.Location = new Point(195, 103);
-            button1.Name = "button1";
-            button1.Size = new Size(226, 216);
-            button1.TabIndex = 1;
-            button1.Text = "Take a Quiz";
-            button1.TextAlign = ContentAlignment.BottomCenter;
-            button1.UseVisualStyleBackColor = false;
+            btn_takequiz.BackColor = SystemColors.Control;
+            btn_takequiz.FlatStyle = FlatStyle.Flat;
+            btn_takequiz.Font = new Font("Segoe UI", 15F);
+            btn_takequiz.Image = Properties.Resources.l_quiz_75px;
+            btn_takequiz.Location = new Point(195, 103);
+            btn_takequiz.Name = "btn_takequiz";
+            btn_takequiz.Size = new Size(226, 216);
+            btn_takequiz.TabIndex = 1;
+            btn_takequiz.Text = "Take a Quiz";
+            btn_takequiz.TextAlign = ContentAlignment.BottomCenter;
+            btn_takequiz.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btn_addword
             // 
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 15F);
-            button2.Image = Properties.Resources.l_add_translation_75px;
-            button2.Location = new Point(554, 103);
-            button2.Name = "button2";
-            button2.Size = new Size(226, 216);
-            button2.TabIndex = 2;
-            button2.Text = "Add a Word";
-            button2.TextAlign = ContentAlignment.BottomCenter;
-            button2.UseVisualStyleBackColor = true;
+            btn_addword.FlatStyle = FlatStyle.Flat;
+            btn_addword.Font = new Font("Segoe UI", 15F);
+            btn_addword.Image = Properties.Resources.l_add_translation_75px;
+            btn_addword.Location = new Point(554, 103);
+            btn_addword.Name = "btn_addword";
+            btn_addword.Size = new Size(226, 216);
+            btn_addword.TabIndex = 2;
+            btn_addword.Text = "Add a Word";
+            btn_addword.TextAlign = ContentAlignment.BottomCenter;
+            btn_addword.UseVisualStyleBackColor = true;
+            btn_addword.Click += btn_addword_Click;
             // 
-            // button3
+            // btn_setting
             // 
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Segoe UI", 15F);
-            button3.Image = Properties.Resources.l_settings_75px;
-            button3.Location = new Point(554, 360);
-            button3.Name = "button3";
-            button3.Size = new Size(226, 216);
-            button3.TabIndex = 4;
-            button3.Text = "Settings";
-            button3.TextAlign = ContentAlignment.BottomCenter;
-            button3.UseVisualStyleBackColor = true;
+            btn_setting.FlatStyle = FlatStyle.Flat;
+            btn_setting.Font = new Font("Segoe UI", 15F);
+            btn_setting.Image = Properties.Resources.l_settings_75px;
+            btn_setting.Location = new Point(554, 360);
+            btn_setting.Name = "btn_setting";
+            btn_setting.Size = new Size(226, 216);
+            btn_setting.TabIndex = 4;
+            btn_setting.Text = "Settings";
+            btn_setting.TextAlign = ContentAlignment.BottomCenter;
+            btn_setting.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btn_wordslist
             // 
-            button4.FlatStyle = FlatStyle.Flat;
-            button4.Font = new Font("Segoe UI", 15F);
-            button4.Image = Properties.Resources.l_words_list_75px;
-            button4.Location = new Point(195, 360);
-            button4.Name = "button4";
-            button4.Size = new Size(226, 216);
-            button4.TabIndex = 3;
-            button4.Text = "Words List";
-            button4.TextAlign = ContentAlignment.BottomCenter;
-            button4.UseVisualStyleBackColor = true;
+            btn_wordslist.FlatStyle = FlatStyle.Flat;
+            btn_wordslist.Font = new Font("Segoe UI", 15F);
+            btn_wordslist.Image = Properties.Resources.l_words_list_75px;
+            btn_wordslist.Location = new Point(195, 360);
+            btn_wordslist.Name = "btn_wordslist";
+            btn_wordslist.Size = new Size(226, 216);
+            btn_wordslist.TabIndex = 3;
+            btn_wordslist.Text = "Words List";
+            btn_wordslist.TextAlign = ContentAlignment.BottomCenter;
+            btn_wordslist.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -104,10 +105,10 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             Controls.Add(label1);
-            Controls.Add(button3);
-            Controls.Add(button4);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(btn_setting);
+            Controls.Add(btn_wordslist);
+            Controls.Add(btn_addword);
+            Controls.Add(btn_takequiz);
             Name = "main_page";
             Size = new Size(980, 681);
             Click += main_page_Click;
@@ -117,10 +118,10 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private Button btn_takequiz;
+        private Button btn_addword;
+        private Button btn_setting;
+        private Button btn_wordslist;
         private Label label1;
     }
 }
--- src/Main Page/main_page.cs
+++ src/Main Page/main_page.cs
@@ -11,25 +11,31 @@ namespace wordz.src.Main_Page
 {
     public partial class main_page : UserControl
     {
-        public main_page()
-        {
-            InitializeComponent();
-        }
-
-        private void main_page_Click(object sender, EventArgs e)
-        {
-           
-        }
-
         // methods
+        private void hide_all_controls()
+        {
+            foreach (Control control in Controls.OfType<Control>())
+            {
+                control.Hide();
+            }
+        }
+
         private void OpenPage(Page_OPT op)
         {
-            switch (op) 
+            hide_all_controls();
+            Control page = null;
+            switch (op)
             {
                 case TAKEQUIZ:
                     // add TAKE QUIZ WINDOW LATER
                     break;
                 case ADDWORD:
+                    page = new Add_word()
+                    {
+                        Name = "addword",
+                        Dock = DockStyle.Fill
+                    };
+                    Util.Push_window((UserControl)page);
 
                     break;
                 case WORDSLIST:
@@ -39,8 +45,23 @@ namespace wordz.src.Main_Page
                     // add SETTING WINDOW LATER
                     break;
             }
+            Controls.Add(page);
+        }
+        public main_page()
+        {
+            InitializeComponent();
+        }
+
+        // main events
+        private void main_page_Click(object sender, EventArgs e)
+        {
 
         }
+        private void btn_addword_Click(object sender, EventArgs e) => OpenPage(ADDWORD);
+
+
+
     }
+        
 
 }
