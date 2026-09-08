4ab65b11ea5906f524c31e0d80b369d126e3ceb1
Mantounio <mahdidev83@gmail.com>
2026-09-08 16:27:12 +0330

    renamed 'add_word' dir to 'Add Word Page'

--- src/Error page/ErrorPage.cs
+++ src/Error page/ErrorPage.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace wordz.src.Error_page
+﻿namespace wordz.src.Error_page
 {
     public partial class ErrorPage : UserControl
     {
