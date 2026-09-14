0054cf60e39afceb84401fac4b98b300f544c03b
Mantounio <mahdidev83@gmail.com>
2026-09-14 21:45:33 +0330

    added word services and repository

--- src/Add Word Page/add_word.cs
+++ src/Add Word Page/add_word.cs
@@ -1,4 +1,6 @@
-﻿namespace wordz.src.add_word
+﻿using wordz.src.Repository;
+
+namespace wordz.src.add_word
 {
     public partial class Add_word : UserControl
     {
@@ -9,6 +11,9 @@
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // make a dbcontext 
+            var wordrepository = new WordsRepository();//
+            var wordservice = new WordService.WordService(wordrepository);
             Util.Page_BackWard();
         }
     }
